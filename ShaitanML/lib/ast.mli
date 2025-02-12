f7bb7eb306c6db41052ee4a698e158b1d1455c63
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2025-02-12 18:04:23 +0300

    some fixes

--- ShaitanML/lib/ast.mli
+++ ShaitanML/lib/ast.mli
@@ -1,8 +1,14 @@
 type id = string
 
+val pp_id : Format.formatter -> id -> unit
+val show_id : id -> string
+
 type rec_flag =
   | Rec
   | Nonrec
+
+val pp_rec_flag : Format.formatter -> rec_flag -> unit
+val show_rec_flag : rec_flag -> string
 
 type const =
   | CInt of int
@@ -10,6 +16,9 @@ type const =
   | CString of id
   | CUnit
   | CNil
+
+val pp_const : Format.formatter -> const -> unit
+val show_const : const -> string
 
 type type_annot =
   | AInt
@@ -21,6 +30,9 @@ type type_annot =
   | ATuple of type_annot list
   | AVar of id
 
+val pp_type_annot : Format.formatter -> type_annot -> unit
+val show_type_annot : type_annot -> string
+
 type pattern =
   | PAny
   | PConst of const
@@ -28,6 +40,9 @@ type pattern =
   | PTuple of pattern list
   | PCons of pattern * pattern
   | PConstraint of pattern * type_annot
+
+val pp_pattern : Format.formatter -> pattern -> unit
+val show_pattern : pattern -> string
 
 type expr =
   | EConst of const
@@ -44,11 +59,23 @@ type expr =
 and case = pattern * expr
 and binding = pattern * expr
 
+val pp_expr : Format.formatter -> expr -> unit
+val show_expr : expr -> string
+val pp_case : Format.formatter -> case -> unit
+val show_case : case -> string
+val pp_binding : Format.formatter -> binding -> unit
+val show_binding : binding -> string
+
 type str_item =
   | SEval of expr
   | SValue of rec_flag * binding list
 
+val pp_str_item : Format.formatter -> str_item -> unit
+val show_str_item : str_item -> string
 val constr_apply : expr -> expr list -> expr
 val constr_fun : pattern list -> expr -> expr
 
 type structure = str_item list
+
+val pp_structure : Format.formatter -> structure -> unit
+val show_structure : structure -> string
